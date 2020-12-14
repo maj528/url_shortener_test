874f447603c1f0704b16abe3e2b855f1510392a3
Majid Fakhoury <majidfakhoury@gmail.com>
2020-12-14 19:10:09 +0100

    models, controllers, and routes all up and running

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module UrlShortenerTest
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework false
+      g.stylesheets false
+      g.helper false
+      g.assets false
+    end
   end
 end
+
+

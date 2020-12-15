485fa8edf9464ce0264484afa26f51f618164ca0
Majid Fakhoury <majidfakhoury@gmail.com>
2020-12-15 19:18:39 +0100

    postgresql

--- db/migrate/20201215152228_remove_click_from_urls.rb
+++
@@ -1,4 +0,0 @@
-class RemoveClickFromUrls < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/migrate/20201215152844_drop_urls.rb
+++ db/migrate/20201215152844_drop_urls.rb
@@ -1,7 +1,5 @@
 class DropUrls < ActiveRecord::Migration[6.0]
   def change
-    drop_table :urls
-
     create_table :urls do |t|
       t.string "long_url"
       t.string "short_url"
